72dc44afc2934e2216d5608b60da39c18bca4455
Andy Yang <500158274@fvtc.edu>
2024-04-19 23:00:20 -0500

    updated app.js, app-routes.js, and secrets

--- app.js
+++ app.js
@@ -1,41 +1,15 @@
-
 const express = require('express')
-const app = express()
 
+const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use('/api/todos', require('./api-routes'))
 app.use(express.static('public'))
-
-const todos = [
-	{ id: 1, item: 'Learn JavaScript', complete: false },
-	{ id: 2, item: 'Learn Express', complete: false },
-	{ id: 3, item: 'Build a To Do App', complete: false }
-]
 
 app.get('/', (_, response) => {
 	response.sendFile('index.html', { root })
 })
-
-app.get('/api/todos', (request, response) => {
-	response.json(todos)
-})
-
-app.post('/api/todos', (request, response) => {
-	const { item } = request.body
-	const id = todos.length + 1
-	const complete = false
-	todos.push({ id, item, complete })
-	response.json({ id })
-})
-
-app.put('/api/todos/:id', (request, response) => {
-	const { id } = request.params
-	const task = todos.find(todo => todo.id.toString() === id)
-	task.complete = !task.complete
-	response.json({ id: task.id, complete: task.complete })
-})
-
 
 const message = `Server running: http://localhost:${port}`
 app.listen(port, () => console.log(message))
